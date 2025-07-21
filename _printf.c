b3bd5c08526dbe7ba0e792656467cc7207706669
Lucas-blancaws <lblancportier@gmail.com>
2025-07-21 11:54:20 +0200

    setup pour start

---
+++ _printf.c
@@ -0,0 +1,7 @@
+#include "main.h"
+
+int _printf(const char *format, ...)
+{
+    va_list args;
+    int count = 0;
+	
---
+++ main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+
+typedef struct format
+{
+    char spec;
+    int (*func)(va_list);
+} format_t;
+
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(va_list args);
+
+int _printf(const char *format, ...);
+int (*get_formatter(char spec))(va_list);
+
+int _putchar(char c);
+int print_number(int n);
+
+#endif
